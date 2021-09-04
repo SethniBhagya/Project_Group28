7ff57de172ee3ca4963b4364f1b06385ce6cc116
SethniBhagya <sethnibhagya67@gmail.com>
2021-09-04 21:56:24 +0530

    changed js

--- Javascript/form.js
+++ Javascript/form.js
@@ -1,30 +1,31 @@
-function validate() {
+// function validate() {
+ 
       
-    if( document.myForm.fname.value == "" ) {
-       alert( "Please provide your name!" );
-       document.myForm.fname.focus() ;
-       return false;
-    }
-    if( document.myForm.lname.value == "" ) {
-        alert( "Please provide your name!" );
-        document.myForm.lname.focus() ;
-        return false;
-     }
-    if( document.myForm.EMail.value == "" ) {
-       alert( "Please provide your Email!" );
-       document.myForm.EMail.focus() ;
-       return false;
-    }
-    if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
-       document.myForm.Zip.value.length != 5 ) {
+//     if( document.myForm.fname.value == "" ) {
+//        alert( "Please provide your name!" );
+//        document.myForm.fname.focus() ;
+//        return false;
+//     }
+//     if( document.myForm.email.value == "" ) {
+//         alert( "Please provide your email!" );
+//         document.myForm.email.focus() ;
+//         return false;
+//      }
+//     if( document.myForm.EMail.value == "" ) {
+//        alert( "Please provide your Email!" );
+//        document.myForm.EMail.focus() ;
+//        return false;
+//     }
+//     if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
+//        document.myForm.Zip.value.length != 5 ) {
        
-       alert( "Please provide a zip in the format #####." );
-       document.myForm.Zip.focus() ;
-       return false;
-    }
-    if( document.myForm.Country.value == "-1" ) {
-       alert( "Please provide your country!" );
-       return false;
-    }
-    return( true );
- }
+//        alert( "Please provide a zip in the format #####." );
+//        document.myForm.Zip.focus() ;
+//        return false;
+//     }
+//     if( document.myForm.Country.value == "-1" ) {
+//        alert( "Please provide your country!" );
+//        return false;
+//     }
+//     return( true );
+//  }
