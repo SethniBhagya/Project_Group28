4221ac463a743d1e5414cd4014bb7b166d742575
Dinusha1234-abcd <dinushagunawarada90@gmail.com>
2021-09-07 15:18:04 +0530

    add

--- wildlifeCare/Config/Database.php
+++ wildlifeCare/Config/Database.php
@@ -2,7 +2,7 @@
   define('DB_TYPE','mysql');
   define('DB_HOST','localhost');
   define('DB_USER','root');
-  define('DB_NAME','db_iee');
+  define('DB_NAME','Wildlife_care');
   define('DB_PASSWORD','');
 
 ?>
