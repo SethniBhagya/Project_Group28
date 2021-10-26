fbec5e1ec712fcf4f185a7ce26def3eeb63722a6
SethniBhagya <sethnibhagya67@gmail.com>
2021-10-26 08:43:50 +0530

    veterinarian pages added

--- APP/views/veterinarian.php
+++ APP/views/veterinarian.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Public/css/veterinarian.css" type="text/css">
-    <link rel="stylesheet" href="../Public/css/wildlifeofficer_header.css" type="text/css">
+    <link rel="stylesheet" href="../Public/css/wildlifeofficerHeader.css" type="text/css">
     <script src="../Public/javascript/login.js"></script>
     <script src="../Public/javascript/wildlifeofficer.js"></script>
     <title>WildlifeCare</title>
@@ -25,6 +25,16 @@
             <ul>
                 <li id="home"><a href="../">HOME</a></li>
                 <li id="dashboard"><a href="../veterinarian/viewDashboard">DASHBOARD</a></li>
+                <li>
+                    <div class="dropdown-1" style="  padding-left:  300px ">
+                        <button class="dropbtn-1">Language</button>
+                        <div class="dropdown-content-1">
+                            <a href="../wildlifeofficer/index?lang=1 ">English</a>
+                            <a href=" ../wildlifeofficer/index?lang=2">සිංහල</a>
+                            <a href=" ">தமிழ்</a>
+                        </div>
+                    </div>
+                </li>
                 <li class="dropdown">
                     <span class="dot"> <img onclick="myFunction_2(this)" src="../Public/images/user_icon.png" id="user_icon" class="user_btn"></span>
                     <div id="myDropdown" class="dropdown-content">
