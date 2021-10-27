5e14d40cf1cf78b79680124d70cee55b07a45c7c
Dinusha1234-abcd <62270984+Dinusha1234-abcd@users.noreply.github.com>
2021-10-27 16:15:30 +0530

    add Tamil Language to view profie one

--- APP/views/report5Tamil.php
+++ APP/views/report5Tamil.php
@@ -36,8 +36,8 @@
                 <li class="dropdown">
                     <span class="dot"> <img onclick="myFunction_2(this)" src="../Public/images/user_icon.png" id="user_icon" class="user_btn"></span>
                     <div id="myDropdown" class="dropdown-content">
-                    <a href="../user/editprofile?lang=3">View Profile</a> 
-                    <a href="../user/logout">Logout</a>
+                    <a href="../user/editprofile?lang=3">சுயவிவரம் காண</a> 
+                    <a href="../user/logout">வெளியேறு</a>
                     </div>
                 </li>
             </ul>
